67fa580a90dd2050a1b47142ae311f8374a80b64
Gaurav <kgaurav9singh@gmail.com>
2019-03-10 17:41:47 +0530

    Initial Commit -  Setting Up

---
+++ src/com/gaurav/springAOP/AOPMain.java
@@ -0,0 +1,17 @@
+package com.gaurav.springAOP;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.gaurav.springAOP.service.ShapeService;
+
+public class AOPMain {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+		ShapeService shapeService = ctx.getBean("shapeService", ShapeService.class);
+		System.out.println(shapeService.getCircle().getName());
+	}
+
+}
+
