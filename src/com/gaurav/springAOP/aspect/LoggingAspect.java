78752d72fd539a1dd4ffeb84d4e19bf59ad5cec1
Gaurav <kgaurav9singh@gmail.com>
2019-03-11 23:25:07 +0530

    Wildcard part 2: Pointcut

--- src/com/gaurav/springAOP/aspect/LoggingAspect.java
+++ src/com/gaurav/springAOP/aspect/LoggingAspect.java
@@ -2,19 +2,24 @@ package com.gaurav.springAOP.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class LoggingAspect {
 
-	@Before("execution(public * get*())")
+	@Before("allGetters()")
 	public void LoggingAdvice() {
 		System.out.println("Advice ran, the get() method called");
 
 	}
 	
-	@Before("execution(public * get*())")
+	@Before("allGetters()")
 	public void secondAdvice() {
 		System.out.println("Second Advice Executed");
+	}
+	
+	@Pointcut("execution(public * get*())")
+	public void allGetters() {
 	}
 
 }
